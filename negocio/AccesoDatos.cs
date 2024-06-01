123e8e70a591c58736a237b5f0f35d0f6c483c15
AllegriM <elmonky99@gmail.com>
2024-06-01 17:55:29 -0300

    conexion a BD arreglada

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -18,7 +18,7 @@ namespace negocio
         }
         public AccesoDatos()
         {
-            conexion = new SqlConnection("server =.\\SQLEXPRESS; database = CATALOGO_P3_DB; integrated security=true");
+            conexion = new SqlConnection("server =.\\SQLEXPRESS; database = TPC_RESTAURANTE_DB; integrated security=true");
             comando = new SqlCommand();
         }
 
